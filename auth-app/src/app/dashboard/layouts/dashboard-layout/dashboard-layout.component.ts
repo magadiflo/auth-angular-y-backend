de343b5678102a53269dc9ceb514393a2fff7345
Martín <magadiflo@gmail.com>
2023-08-10 11:42:57 -0500

    Estructura del proyecto

---
+++ auth-app/src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  templateUrl: './dashboard-layout.component.html',
+  styles: [
+  ]
+})
+export class DashboardLayoutComponent {
+
+}
